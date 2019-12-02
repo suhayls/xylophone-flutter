7a24fe4494d1bee6bd592f4c7c10678424c612fe
Suhayl <suhayls@gmail.com>
2019-12-02 14:25:53 +0800

    Complete xylophone app with multiple sounds playing inside function

--- lib/main.dart
+++ lib/main.dart
@@ -4,20 +4,39 @@ import 'package:audioplayers/audio_cache.dart';
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  void playSound(int soundNumber) {
+    final player = AudioCache();
+    player.play('note$soundNumber.wav');
+  }
+
+  Expanded buildKey({int soundNumber, Color keyColor}) {
+    return Expanded(
+      child: FlatButton(
+        onPressed: () {
+          playSound(soundNumber);
+        },
+        color: keyColor,
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Center(
-            child: FlatButton(
-              onPressed: () {
-                final player = AudioCache();
-                player.play('note1.wav');
-              },
-              child: Text('Click Me'),
-            ),
-          ),
+          child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                buildKey(soundNumber: 1, keyColor: Colors.red),
+                buildKey(soundNumber: 2, keyColor: Colors.orange),
+                buildKey(soundNumber: 3, keyColor: Colors.yellow),
+                buildKey(soundNumber: 4, keyColor: Colors.green),
+                buildKey(soundNumber: 5, keyColor: Colors.teal),
+                buildKey(soundNumber: 6, keyColor: Colors.blue),
+                buildKey(soundNumber: 7, keyColor: Colors.purple),
+              ]),
         ),
       ),
     );
